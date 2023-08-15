feccd6826e09ed535ab6c6e65cc423aa4a8bd0d2
yasoob <yasoob@users.noreply.github.com>
2023-08-15 00:16:51 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="yasoob"
+original_name="codebolt"
+original_urlname="codebolt"
+original_description="Awesome codebolt created by yasoob"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv codebolt $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
